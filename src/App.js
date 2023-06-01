972e03d69ba03e5b8c5a897e4d9d177da9fbc918
Karina Velasquez <kvelasquez@xpanxion.com>
2023-05-31 21:33:48 -0400

    Hw2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import{ QueryClient, QueryClientProvider } from "react-query";
+import{ ReactQueryDevTools } from "react-query-devtools";
+import Repositories from "./Repositories"
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+const queryClient = new QueryClient({});
+
+const App = () =>{
+  return(
+    <QueryClientProvider client={queryClient}>
+       <Repositories/>
+      {/* <ReactQueryDevTools initialIsOpen={true}/> */}
+    </QueryClientProvider>
   );
-}
+};
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+        // <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
